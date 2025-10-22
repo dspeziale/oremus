6750cc91064d2d751d973c93f706494f922a487d
Daniele.Speziale <dspeziale@gmail.com>
2025-10-22 15:54:32 +0200

    22/10 Pomeriggio

--- t.py
+++ t.py
@@ -9,7 +9,7 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('master.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=59000)
